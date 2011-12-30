2f5607f443dd93c52a0077020add0d22d2a253e6
Piotr Banaszkiewicz <piotr@banaszkiewicz.org>
2011-12-30 17:43:14 +0100

    New updating script!

---
+++ update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# DISCLAIMER:
+# This installation script is intended not to do any harm, but the author makes
+# no warranty that it certainly won't and/or perfectly fits your needs.
+
+CURR_DIR="`pwd`"
+BUNDLE_DIR="$CURR_DIR/bundle"
+
+echo "Updating all addons..."
+
+for i in `ls $BUNDLE_DIR`
+do
+    echo "Updating $i..."
+    cd "$BUNDLE_DIR/$i"
+    git pull origin >/dev/null 2>&1
+done
